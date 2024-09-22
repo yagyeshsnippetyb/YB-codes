902c3300c69fe697ba36ec5197fa75ac4f3825ac
Yagyesh <bhardwajyagyesh192@gmail.com>
2024-09-22 16:18:13 +0530

    first commit

---
+++ array.c
@@ -0,0 +1,22 @@
+ #include <stdio.h>
+ struct student {
+    char name[20];
+    int age;
+    float gpa;
+    };
+ int main(){
+    struct student students[2] = {
+        {"Yagyesh", 18, 9.1},
+        {"Nitish", 21, 8.9}
+    };
+
+    for (int i = 0; i < 2; i++){
+        printf("student %d\n" , i+1);
+        printf("name: %s\n" , students[i].name);
+        printf("age: %d\n" , students[i].age);
+        printf("CGPA:%f\n" , students[i].gpa);
+        printf("\n");
+    }
+
+    return 0;
+ }
